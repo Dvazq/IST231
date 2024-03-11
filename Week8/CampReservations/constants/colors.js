d54fb9e7653110944772382203a0c90d4271d0c9
unknown <danielv4zquez@gmail.com>
2024-03-10 20:29:25 -0400

    Add Camp Reservations

---
+++ Week8/CampReservations/constants/colors.js
@@ -0,0 +1,10 @@
+const Colors = {
+  accent300: "#6caacd",
+  accent500: "#007ba7",
+  primary300o5: "rgba(245, 245, 220, 0.5)",
+  primary300: "#313131",
+  primary500: "#26ff00",
+  primary800: "#8b4513"
+};
+
+export default Colors;
